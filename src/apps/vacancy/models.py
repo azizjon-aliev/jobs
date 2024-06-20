7fb5379f38d866cd7f37659c897f13462ceb75c5
Azizjon <azizaliev2337@gmail.com>
2024-06-20 21:06:43 +0500

    adding uuid

--- src/apps/vacancy/models.py
+++ src/apps/vacancy/models.py
@@ -1,30 +1,50 @@
+import uuid
+
 from django.db import models
 
 
 class Vacancy(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    salary = models.FloatField()
-    company = models.ForeignKey('Company', on_delete=models.CASCADE)
-    skills = models.ManyToManyField('Skill', related_name='vacancies')
+	id = models.UUIDField(
+		default=uuid.uuid4,
+		unique=True,
+		primary_key=True,
+		editable=False
+	)
+	title = models.CharField(max_length=255)
+	description = models.TextField()
+	salary = models.FloatField()
+	company = models.ForeignKey('Company', on_delete=models.CASCADE)
+	skills = models.ManyToManyField('Skill', related_name='vacancies')
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    city = models.CharField(max_length=255)
-    address = models.TextField()
+	id = models.UUIDField(
+		default=uuid.uuid4,
+		unique=True,
+		primary_key=True,
+		editable=False
+	)
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	city = models.CharField(max_length=255)
+	address = models.TextField()
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+	id = models.UUIDField(
+		default=uuid.uuid4,
+		unique=True,
+		primary_key=True,
+		editable=False
+	)
+	name = models.CharField(max_length=255)
+	description = models.TextField()
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
